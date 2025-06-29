bb093e875219617ac419c06f43ea387f70811963
jonathan.dalthon <estagiario@elcoma.com.br>
2025-06-29 12:00:44 -0300

    separação prod e dev

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,10 +15,5 @@ python scripts/check_tables.py
 echo "Verificando se é necessário criar tabelas..."
 python scripts/setup_db.py
 
-#echo "Limpando as tabelas..."
-#python scripts/clear_data.py
-
-#echo "Populando banco com dados mockados..."
-#python scripts/seed_data.py
 
 uvicorn app.main:app --host 0.0.0.0 --port $PORT
